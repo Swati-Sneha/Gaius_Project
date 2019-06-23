8762e29533632216a5bc83527250c0a04ab8ec64
swati <swati.sneha095@gmail.com>
2019-06-23 14:57:12 +0530

    Comments updated

--- tasks.py
+++ tasks.py
@@ -34,19 +34,20 @@ class ImageHandler:
 		new_image_name=str(self.image_name.split(".")[0])+"_orig_."+str(self.image_name.split(".")[-1])
 		print(self.output_dir+new_image_name)
 		self.im.save(self.output_dir+new_image_name)
-		time.sleep(5)
+		time.sleep(5) #Explicitly given to mimic time-consumic jobs
 
 	def convert_mid_resolution(self):
 		'''saves the image in output_directory in medium resolution'''
 		new_image = self.im.resize((int(self.width/2), int(self.height/2)))
 		new_image_name=str(self.image_name.split(".")[0])+"_mid_"+str(self.width/2)+"."+str(self.image_name.split(".")[-1])  #Can further be modified for better nomenclature
 		new_image.save(self.output_dir+new_image_name)
-		time.sleep(10)
+		time.sleep(10)#Explicitly given to mimic time-consumic jobs
 
 	def convert_low_resolution(self):
 		'''saves the image in output_directory in low resolution'''
 		new_image = self.im.resize((int(self.width/4), int(self.height/4)))
 		new_image_name=str(self.image_name.split(".")[0])+"_low_"+str(self.width/4)+"."+str(self.image_name.split(".")[-1]) #Can further be modified for better nomenclature
 		new_image.save(self.output_dir+new_image_name)
-		time.sleep(10)
+		time.sleep(10)#Explicitly given to mimic time-consumic jobs
+
 
