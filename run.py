9e81ee09f9a7627084e9fc05c1744c6c8f75a053
swati <swati.sneha095@gmail.com>
2019-06-23 14:44:58 +0530

    Flask dynamic url updated

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from tasks import ImageHandler
 from flask import Flask, request 
 import threading
 from configparser import ConfigParser
+import glob, random
 
 queue = mp.Queue()
 app = Flask(__name__) 
@@ -26,14 +27,28 @@ image_2=config.get('images', 'image_2')
 OUTPUT_DIRECTORY=os.path.join(os.path.dirname(__file__), output_dir)
 DATA_DIRECTORY=os.path.join(os.path.dirname(__file__), input_dir)
 
+
+images=glob.glob(DATA_DIRECTORY+'*.png')
+len_images=len(images)
+# images_int=range(len_images)
+
+print(images)
+
+@app.route('/read_photo/<var>', methods=['GET'])
+def read_photo(var):
+	image=DATA_DIRECTORY+random.choice(images)
+	add_task(image, 'image') 
+	return 'Image of random choice read successfully '+str(image.split('/')[-1])
+
+
 @app.route('/read_photo') 
-def read_photo():         
+def read_photo1():         
 	'''read photo from a path, this can be changed to upload phot in further course'''
 	image=DATA_DIRECTORY+image_1
 	add_task(image, 'image')             
 	return 'Image 1 Read Successfully..'
 
-@app.route('/read_photo2')
+@app.route('/read_photo1')
 def read_photo2():
 	'''read another photo from path, to check multiple requests, further can be changed to upload video/audio'''
 	image=DATA_DIRECTORY+image_2
